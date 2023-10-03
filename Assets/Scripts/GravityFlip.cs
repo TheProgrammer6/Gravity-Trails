3f89b12d0558384e1f58f44969dc9163a00035cc
Jason <858581@pdsb.net>
2023-10-03 18:50:15 -0400

    implemented gravity

---
+++ Assets/Scripts/GravityFlip.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityFlip : MonoBehaviour
+{
+    public Rigidbody2D avatarRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            //setting gravity to opposite (up)
+            avatarRigidbody.gravityScale *= -1;
+            //saving the current scale to variable newDirection
+            Vector3 newDirection = transform.localScale;
+            //changing the y property of the newDirection variable
+            newDirection.y *= -1;
+            //giving the new scale property with the changed Y scale to the character
+            gameObject.transform.localScale = newDirection;
+        }
+    }
+}
